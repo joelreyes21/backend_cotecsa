9e91668f3596a922c432e673a5f4850a17367802
joelreyes21 <jr4419543@gmail.com>
2026-02-09 00:45:55 -0600

    Update db.js
    asd

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql2");
 
+console.log("📌 Variables recibidas por Railway:");
+console.log("HOST:", process.env.MYSQLHOST);
+console.log("USER:", process.env.MYSQLUSER);
+console.log("DB:", process.env.MYSQLDATABASE);
+console.log("PORT:", process.env.MYSQLPORT);
+
 const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE,
+  database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT
 });
 

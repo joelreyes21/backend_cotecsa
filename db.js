f56bdc6bd048bf36f8ef314f0aa6dc4b1ea69b46
joelreyes21 <jr4419543@gmail.com>
2026-02-09 00:51:10 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,14 @@
 const mysql = require("mysql2");
 
-console.log("📌 Variables recibidas por Railway:");
-console.log("HOST:", process.env.MYSQLHOST);
-console.log("USER:", process.env.MYSQLUSER);
-console.log("DB:", process.env.MYSQLDATABASE);
-console.log("PORT:", process.env.MYSQLPORT);
-
 const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.getConnection((err, connection) => {

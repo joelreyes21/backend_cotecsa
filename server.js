ad36e4e70d3b897920e9d95d4c72668b3572cfc1
joelreyes21 <jr4419543@gmail.com>
2026-02-15 21:46:59 -0600

    Update server.js
    asd

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const bcrypt = require("bcryptjs");
 const db = require("./db");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;

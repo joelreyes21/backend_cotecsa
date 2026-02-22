c3f81197bbdfc672878949ead42b786b0ed46a66
joelreyes21 <jr4419543@gmail.com>
2026-02-21 21:15:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -24,10 +24,15 @@ const PORT = process.env.PORT || 3000;
 ========================= */
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true solo si es 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
